a8aa7b2377422d346b3c04fe5c8471489a71fa5c
bkumarbasant7 <bkumarbasant@gmail.com>
2020-01-08 19:53:57 +0530

    not working

--- app/src/main/java/com/example/demoimagegrid/Food.kt
+++ app/src/main/java/com/example/demoimagegrid/Food.kt
@@ -1,10 +1,12 @@
 package com.example.demoimagegrid
 
+import android.net.Uri
+
 class Food {
     var name: String? = null
-    var image: Int? = null
+    var image: Uri? = null
 
-    constructor(name: String, image: Int) {
+    constructor(name: String, image: Uri) {
         this.name = name
         this.image = image
     }
--- app/src/main/java/com/example/demoimagegrid/MainActivity.kt
+++ app/src/main/java/com/example/demoimagegrid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.demoimagegrid
 
 import android.Manifest
+import android.R.attr
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
@@ -9,7 +10,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
@@ -17,16 +17,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.image_item.*
 import kotlinx.android.synthetic.main.image_item.view.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
     var adapter: FoodAdapter? = null
     var foodsList = ArrayList<Food>()
-
     //for floating action button to open camera and permission
     var PERMISSION_CODE: Int = 1000
     var IMAGE_CAPTURE_CODE: Int = 1001
@@ -37,7 +38,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //*****************capture button logic****************************
         capture_btn.setOnClickListener {
-            //if system os is Marshmallow or Above, we need to request runtime permission
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.CAMERA)
                     == PackageManager.PERMISSION_DENIED ||
@@ -60,17 +60,12 @@ class MainActivity : AppCompatActivity() {
         }
         //*****************************************************************
         // load foods
-        foodsList.add(Food("Coffee", R.drawable.coffee_pot))
-        foodsList.add(Food("Espersso", R.drawable.espresso))
-        foodsList.add(Food("French Fires", R.drawable.french_fries))
-        foodsList.add(Food("Honey", R.drawable.honey))
-        foodsList.add(Food("Strawberry", R.drawable.strawberry_ice_cream))
-        foodsList.add(Food("Sugar cubes", R.drawable.sugar_cubes))
-        adapter = FoodAdapter(this, foodsList)
 
+        adapter = FoodAdapter(this, foodsList)
         gvImages.adapter = adapter
     }
-    private fun openCamera(){
+
+    private fun openCamera() {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
@@ -80,34 +75,42 @@ class MainActivity : AppCompatActivity() {
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
-
-    //
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         //called when user presses ALLOW or DENY from Permission Request Popup
-        when(requestCode){
+        when (requestCode) {
             PERMISSION_CODE -> {
                 if (grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     //permission from popup was granted
                     openCamera()
-                }
-                else{
+                } else {
                     //permission from popup was denied
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
+
     @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         //called when image was captured from camera intent
-        if (resultCode == Activity.RESULT_OK){
+        if (resultCode == Activity.RESULT_OK) {
             //set image captured to image view
-            imgFood.setImageURI(image_uri)
+            if(data!= null){
+                val photoUri: Uri =data.getData() as Uri
+                foodsList.add(Food("a",photoUri))
+                adapter = FoodAdapter(this, foodsList)
+                gvImages.adapter = adapter
+            }
+            //imgFood.setImageURI(image_uri)
+
         }
     }
-
 
     class FoodAdapter : BaseAdapter {
         var foodsList = ArrayList<Food>()
@@ -129,14 +132,13 @@ class MainActivity : AppCompatActivity() {
         override fun getItemId(position: Int): Long {
             return position.toLong()
         }
-
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             val food = this.foodsList[position]
 
             var inflator =
                 context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             var foodView = inflator.inflate(R.layout.image_item, null)
-            foodView.imgFood.setImageResource(food.image!!)
+            foodView.imgFood.setImageURI(food.image)
             foodView.tvName.text = food.name!!
 
             return foodView
